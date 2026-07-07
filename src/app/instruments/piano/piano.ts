545ee3bbf7dc64fb2db3f4e829bae3b4c0ea98e7
Joao-Pedro-Ferreira-Felix <jpedro34567890@gmail.com>
2026-07-07 16:48:27 -0300

    Piano highlighting improved

--- src/app/instruments/piano/piano.ts
+++ src/app/instruments/piano/piano.ts
@@ -13,70 +13,87 @@ export class Piano {
     switch (event.keyCode) {
     case 49: // "1"
       this.playNote(0);
+      this.highlightNote(0);
       break;
 
     case 83: // "s"
       this.playNote(1);
+      this.highlightNote(1);
       break;
 
     case 50: // "2"
       this.playNote(2);
+      this.highlightNote(2);
       break;
 
     case 68: // "d"
       this.playNote(3);
+      this.highlightNote(3);
       break;
 
     case 51: // "3"
       this.playNote(4);
+      this.highlightNote(4);
       break;
 
     case 52: // "4"
       this.playNote(5);
+      this.highlightNote(5);
       break;
 
     case 70: // "f"
       this.playNote(6);
+      this.highlightNote(6);
       break;
 
     case 53: // "5"
       this.playNote(7);
+      this.highlightNote(7);
       break;
 
     case 71: // "g"
       this.playNote(8);
+      this.highlightNote(8);
       break;
 
     case 54: // "6"
       this.playNote(9);
+      this.highlightNote(9);
       break;
 
     case 72: // "h"
       this.playNote(10);
+      this.highlightNote(10);
       break;
 
     case 55: // "7"
       this.playNote(11);
+      this.highlightNote(11);
       break;
 
     case 56: // "8"
       this.playNote(12);
+      this.highlightNote(12);
       break;
 
     case 74: // "j"
       this.playNote(13);
+      this.highlightNote(13);
       break;
 
     case 57: // "9"
       this.playNote(14);
+      this.highlightNote(14);
       break;
 
     case 75: // "k"
       this.playNote(15);
+      this.highlightNote(15);
       break;
 
     case 48: // "0"
       this.playNote(16);
+      this.highlightNote(16);
       break;
     }
   }
@@ -102,8 +119,12 @@ export class Piano {
     new Audio('audio/piano-notes/17-E.mp3')
   ];
   playNote(index: number) {
+    for (let i: number = 0; i < this.noteCssClass.length; ++i) { this.unhighlightNote(i); } // Unhighlights all keys whenever a key is pressed (this ensures only 1 is highlighted at a time)
+
     this.audio[index].currentTime = 0;
     this.audio[index].play();
+
+    this.audio[index].onended = (event: Event) => { console.log('note ended'); this.unhighlightNote(index); console.log(this.noteCssClass); }; // TODO: THIS RIGHT HERE IS WORKING, JUST NOT REFRESHING
   }
 
   displayLetters: boolean = true;
